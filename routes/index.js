008d73c1af066bd74a2d8bbcff514e9fd53e6d8f
Sujith K <sujithk1910@gmail.com>
2021-06-11 13:06:15 +0530

    added as for preload

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
     const products = await Product.find({})
       .sort("-createdAt")
       .populate("category");
-    res.links({preload: "https://ka-f.fontawesome.com/releases/v5.15.3/webfonts/free-fa-solid-900.woff2"});
+    res.links({preload: 'https://ka-f.fontawesome.com/releases/v5.15.3/webfonts/free-fa-solid-900.woff2; as="font"'});
     res.render("shop/home", { pageName: "Home", products });
   } catch (error) {
     console.log(error);
